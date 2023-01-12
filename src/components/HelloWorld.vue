f87feaadd00e79f469fe2bd8d1c6c3d751719ab0
yutounun <geek.yuto@gmail.com>
2023-01-13 07:57:54 +0900

    Play with grid-system

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,61 @@
 <template>
   <div>
-    <div class="flex flex-row">
+    <div class="flex flex-row mb-10">
       <span class="mr-2 w-10 bg-blue-400 text-center rounded-full">1</span>
       <span class="mr-2 w-10 bg-blue-400 text-center rounded-full">2</span>
       <span class="mr-2 w-10 bg-blue-400 text-center rounded-full">3</span>
+    </div>
+    <div class="grid grid-cols-4 gap-20 ma-5">
+      <div
+        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+      >
+        01
+      </div>
+      <div
+        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+      >
+        01
+      </div>
+      <div
+        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+      >
+        01
+      </div>
+      <div
+        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+      >
+        01
+      </div>
+      <div
+        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+      >
+        01
+      </div>
+      <div
+        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+      >
+        01
+      </div>
+      <div
+        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+      >
+        01
+      </div>
+      <div
+        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+      >
+        01
+      </div>
+      <div
+        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+      >
+        01
+      </div>
+      <div
+        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+      >
+        01
+      </div>
     </div>
   </div>
 </template>
