920bcab3a9b1d70d065f3e615600ee0171a4d01a
yutounun <geek.yuto@gmail.com>
2023-01-14 09:58:11 +0900

    Play with grid

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,54 +5,59 @@
       <span class="mr-2 w-10 bg-blue-400 text-center rounded-full">2</span>
       <span class="mr-2 w-10 bg-blue-400 text-center rounded-full">3</span>
     </div>
-    <div class="grid grid-cols-4 gap-20 ma-5">
+    <div class="grid grid-cols-4 gap-20 justify-items-center ma-5">
       <div
-        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+        class="text-white h-10 w-60 flex justify-center items-center bg-blue-400 rounded-md"
       >
         01
       </div>
       <div
-        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+        class="text-white h-10 w-60 flex justify-center items-center bg-blue-400 rounded-md"
       >
         01
       </div>
       <div
-        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+        class="text-white h-10 w-60 flex justify-center items-center bg-blue-400 rounded-md"
       >
         01
       </div>
       <div
-        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+        class="text-white h-10 w-60 flex justify-center items-center bg-blue-400 rounded-md"
       >
         01
       </div>
       <div
-        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+        class="text-white h-10 w-60 flex justify-center items-center bg-blue-400 rounded-md"
       >
         01
       </div>
       <div
-        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+        class="text-white h-10 w-60 flex justify-center items-center bg-blue-400 rounded-md"
       >
         01
       </div>
       <div
-        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+        class="text-white h-10 w-60 flex justify-center items-center bg-blue-400 rounded-md"
       >
         01
       </div>
       <div
-        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+        class="text-white h-10 w-60 flex justify-center items-center bg-blue-400 rounded-md"
       >
         01
       </div>
       <div
-        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+        class="text-white h-10 w-60 flex justify-center items-center bg-blue-400 rounded-md"
       >
         01
       </div>
       <div
-        class="text-white h-10 flex justify-center items-center bg-blue-400 rounded-md"
+        class="text-white h-10 w-60 flex justify-center items-center bg-blue-400 rounded-md"
+      >
+        01
+      </div>
+      <div
+        class="text-white h-10 w-60 flex justify-center items-center bg-blue-400 rounded-md"
       >
         01
       </div>
